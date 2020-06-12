872caf257f0860084272a3ebb5b1598993228840
iman malekian <imanmalekian31@gmail.com>
2020-06-12 12:51:39 +0430

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 			},
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'icon', type: 'image/x-icon', href: '/logo.jpg' },
 			{
 				rel: 'stylesheet',
 				href:
